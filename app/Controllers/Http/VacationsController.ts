2502c495648fd793df8140f5083ca55c31fe2899
Daniel Flores <dflores@i4digital.com>
2023-07-26 15:45:40 -0500

    ajuste

--- app/Controllers/Http/VacationsController.ts
+++ app/Controllers/Http/VacationsController.ts
@@ -1,13 +1,11 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import { EResponseCodes } from "App/Constants/ResponseCodesEnum";
 import { ApiResponse } from "App/Utils/ApiResponses";
 import VacationProvider from "@ioc:core.VacationProvider";
 import CreateAndUpdateVacationValidator from "App/Validators/CreateAndUpdateVacationValidator";
-import { IVacation } from 'App/Interfaces/VacationsInterfaces';
-
+import { IVacation } from "App/Interfaces/VacationsInterfaces";
 
 export default class VacationsController {
-
   // get all vactions
   public async getVacations({ response }: HttpContextContract) {
     try {
@@ -24,7 +22,7 @@ export default class VacationsController {
     try {
       const vacation = request.body() as IVacation;
       //const vacation = await request.validate(CreateAndUpdateVacationValidator)
-      return response.send(await VacationProvider.createVacation(vacation))
+      return response.send(await VacationProvider.createVacation(vacation));
     } catch (err) {
       return new ApiResponse(null, EResponseCodes.FAIL, String(err.messages));
     }
@@ -34,11 +32,10 @@ export default class VacationsController {
   public async updateVacation({ request, response }: HttpContextContract) {
     try {
       const { id } = request.params();
-      const vacation = await request.validate(CreateAndUpdateVacationValidator)
-      return response.send(await VacationProvider.updateVacation(vacation, id))
+      const vacation = await request.validate(CreateAndUpdateVacationValidator);
+      return response.send(await VacationProvider.updateVacation(vacation, id));
     } catch (err) {
-      new ApiResponse(null, EResponseCodes.FAIL, String(err));
+      return new ApiResponse(null, EResponseCodes.FAIL, String(err));
     }
   }
-  
 }

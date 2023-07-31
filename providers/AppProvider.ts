6add56cc0a13439d5cdb0cc3385b1eef6370a976
Sebastian Medina <jmedina@i4digital.com>
2023-07-31 11:42:36 -0500

    Resolución de conflictos de Merge - Provider

--- providers/AppProvider.ts
+++ providers/AppProvider.ts
@@ -12,10 +12,7 @@ export default class AppProvider {
     const VinculationProvider = await import("App/Services/VinculationService");
     const VacationService = await import("App/Services/VacationService");
     const IncapacityService = await import("App/Services/IncapacityService");
-<<<<<<< HEAD
-=======
 
->>>>>>> S3-NOM014
     /**************************************************************************/
     /************************ EXTERNAL SERVICES ********************************/
     /**************************************************************************/
@@ -24,35 +21,12 @@ export default class AppProvider {
     /******************************** REPOSITORIES ****************************/
     /**************************************************************************/
     const WorkerRepository = await import("App/Repositories/WorkerRepository");
-<<<<<<< HEAD
-    const IncapacityRepository = await import(
-      "App/Repositories/IncapacityRepository"
-    );
-    const VacationRepository = await import(
-      "App/Repositories/VacationRepository"
-    );
-    const EmploymentRepository = await import(
-      "App/Repositories/EmploymentRepository"
-    );
-
-    const RelativeRepository = await import(
-      "App/Repositories/RelativeRepository"
-    );
-
-    const ChargesRepository = await import(
-      "App/Repositories/ChargesRepository"
-    );
-    const TypesContractsRepository = await import(
-      "App/Repositories/TypesContractsRepository"
-    );
-=======
     const VacationRepository = await import("App/Repositories/VacationRepository");
     const EmploymentRepository = await import("App/Repositories/EmploymentRepository");
     const RelativeRepository = await import("App/Repositories/RelativeRepository");
     const ChargesRepository = await import("App/Repositories/ChargesRepository");
     const TypesContractsRepository = await import("App/Repositories/TypesContractsRepository");
     const IncapacityRepository = await import("App/Repositories/IncapacityRepository");
->>>>>>> S3-NOM014
 
     /**************************************************************************/
     /******************************** CORE  ***********************************/
@@ -79,10 +53,7 @@ export default class AppProvider {
       "core.IncapacityProvider",
       () => new IncapacityService.default(new IncapacityRepository.default())
     );
-<<<<<<< HEAD
-=======
 
->>>>>>> S3-NOM014
   }
 
   public async boot() {

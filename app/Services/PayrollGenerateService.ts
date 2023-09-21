b4761d08b3211f0cd36408a997175d54b7225fa9
Daniel Flores <dflores@i4digital.com>
2023-09-21 17:20:59 -0500

    ajustes

--- app/Services/PayrollGenerateService.ts
+++ app/Services/PayrollGenerateService.ts
@@ -4,6 +4,7 @@ import { EResponseCodes } from "../Constants/ResponseCodesEnum";
 import { IPayrollGenerateRepository } from "../Repositories/PayrollGenerateRepository";
 import FormsPeriodRepository from "App/Repositories/FormsPeriodRepository";
 import { IFormPeriod } from "App/Interfaces/FormPeriodInterface";
+import { IEmployment } from "App/Interfaces/EmploymentInterfaces";
 
 export interface IPayrollGenerateService {
   payrollGenerateById(id: number): Promise<ApiResponse<boolean>>;
@@ -18,11 +19,14 @@ export default class PayrollGenerateService implements IPayrollGenerateService {
   async payrollGenerateById(id: number): Promise<ApiResponse<boolean>> {
     const formPeriod = await this.formsPeriodRepository.getFormPeriodById(id);
 
-    // Validar si la planilla esta autorizada o no existe el period
+    // 1. Validar si la planilla esta autorizada o no existe el periodo
     if (!formPeriod || formPeriod.length === 0) {
       return new ApiResponse(false, EResponseCodes.FAIL, "....");
     }
 
+    // 2. Elimina todos los elemento calculados (Historico, Reservas, Ingresos ...)
+
+    // 3. Genera la planilla segun el tipo
     switch (formPeriod[1].idFormType) {
       case 1: // Planilla Quincenal
         await this.generatePayrollBiweekly(formPeriod[1]);
@@ -35,16 +39,51 @@ export default class PayrollGenerateService implements IPayrollGenerateService {
     return new ApiResponse(true, EResponseCodes.OK);
   }
 
-  async generatePayrollBiweekly(formsPeriod: IFormPeriod): Promise<void> {
-
-    //1. buscar los empelados activos de la planilla quincenal. 
-
-    
+  async generatePayrollBiweekly(formPeriod: IFormPeriod): Promise<void> {
+    //buscar los empelados activos de la planilla quincenal.
 
 
 
 
+    const emploments = await this.payrollGenerateRepository.getActiveEmploments(
+      1,
+      new Date(String(formPeriod.dateEnd))
+    );
 
+    Promise.all(
+      emploments.map(async (emploment) => {
+        try {
+          
+
+
+          // 1. Calcula Licencia
+          await this.calculateLicense(emploment, formPeriod);
+
+          // 2. Calcula Licencia
+          await this.calculateIncapacity(emploment, formPeriod);
+        } catch (error) {
+          // Crea historico Fallido
+        }
+      })
+    );
+  }
+
+  async calculateLicense(
+    employment: IEmployment,
+    formPeriod: IFormPeriod
+  ): Promise<void> {
+    // 1. buscar liciencias vigentes y que entren en planilla
+    // 2. si no exitiste return
+    // 3. Calcula e inserta en la tabla final de Ingresos
+  }
+
+  async calculateIncapacity(
+    employment: IEmployment,
+    formPeriod: IFormPeriod
+  ): Promise<void> {
+    // 1. buscar incapacidades vigentes y que entren en planilla
+    // 2. si no exitiste return
+    // 3. Calcula e inserta en la tabla final de Ingresos
   }
 }
 

74dcdae9c744ec55454d3cf98e4921bbd9261db0
Mauricio Betancur <jmbetancur@i4digital.com>
2023-09-22 16:10:41 -0500

    Buscador agrupador

--- app/Repositories/PayrollGenerateRepository.ts
+++ app/Repositories/PayrollGenerateRepository.ts
@@ -1,11 +1,14 @@
 import { IEmployment } from "App/Interfaces/EmploymentInterfaces";
+import { IGrouper } from "App/Interfaces/GrouperInterfaces";
 import Employment from "App/Models/Employment";
+import Grouper from "App/Models/Grouper";
 
 export interface IPayrollGenerateRepository {
   getActiveEmploments(
     contractType: number,
     endDate: Date
   ): Promise<IEmployment[]>;
+  getByIdGrouper(id: number): Promise<IGrouper>;
 }
 export default class PayrollGenerateRepository
   implements IPayrollGenerateRepository
@@ -19,5 +22,11 @@ export default class PayrollGenerateRepository
     const res = await Employment.query().preload("worker");
 
     return res.map((i) => i.serialize() as IEmployment);
+  }
+
+  async getByIdGrouper(id: number): Promise<IGrouper> {
+    const res = await Grouper.findByOrFail("id", id);
+
+    return res.serialize() as IGrouper;
   }
 }

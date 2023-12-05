ac254755f04071f1de939f975b006cd22a6f42c2
Mauricio Betancur <jmbetancur@i4digital.com>
2023-12-05 09:04:25 -0500

    Ajuste excel

--- app/Services/ReportsService.ts
+++ app/Services/ReportsService.ts
@@ -127,7 +127,8 @@ export default class ReportService implements IReportService {
         "Salario Base": historical.salary,
         "Total ingresos": historical.totalIncome,
         "Total deducciones": historical.totalDeduction,
-        Total: historical.total,
+        Total: historical.totalIncome - historical.totalDeduction,
+        // total: historical.total
       };
 
       incomeTypeToShow.forEach((iType) => {
